577e93d4555e7fd5fc50c338e965789d21dddcde
Ben Adenle <datapitch@gmail.com>
2024-06-15 08:51:45 +0100

    map external wackitravels links

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        {/**Public Routes */}
+        {/** Public Routes */}
         <Route
           index
           element={
@@ -53,6 +53,44 @@ function App() {
             <SuspenseWrapper>
               <ContactPage />
             </SuspenseWrapper>
+          }
+        />
+
+        {/** External Links with target="_blank" */}
+        <Route
+          path="/wackitravels"
+          element={
+            <a
+              href="https://wackiitravels.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wacki Travels
+            </a>
+          }
+        />
+        <Route
+          path="/wackitravels/news"
+          element={
+            <a
+              href="https://wackiitravels.com/news"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wacki Travels News
+            </a>
+          }
+        />
+        <Route
+          path="/wackitravels/login"
+          element={
+            <a
+              href="https://wackiitravels.com/login"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wacki Travels Login
+            </a>
           }
         />
       </Route>
